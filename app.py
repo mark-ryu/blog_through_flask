9bea865a4b71c94d5651b48229772ff281e8f6de
markryu <atienza.new@gmail.com>
2019-02-06 18:21:15 +0800

    started with bootstrap navbar

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Arvin',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'Feb 06, 2019'
+    },
+    {
+        'author': 'Mark',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'Feb 06, 2019'
+    }
+]
+
 @app.route("/")
 def index():
-    return "Hello World"
+    return render_template('index.html', posts=posts, title='HOME')
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='ABOUT PAGE')
 
 if __name__ == "__main__":
     app.run(debug=True)
