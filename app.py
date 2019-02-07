2d40ddf6bdb432b724d47d1c45dbb2ec897a55c3
l <l@localhost.localdomain>
2019-02-07 23:17:14 +0800

    login test unsuccessful

--- app.py
+++ app.py
@@ -36,9 +36,15 @@ def register():
         return redirect(url_for('index'))
     return render_template('register.html', form=form, title='Register')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.username.data == "sample@gmail.com" and form.password.data == "password":
+            flash(f'Logged in as {form.username.data}', 'success')
+            return redirect(url_for('index'))
+        else:
+            flash('Login Unsuccessful, try again.', 'danger')
     return render_template('login.html', form=form, title='Login')
 
 
