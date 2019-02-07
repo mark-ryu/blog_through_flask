315d9dd95966f19f49803e549fe92bfff55043e7
l <l@localhost.localdomain>
2019-02-07 21:31:57 +0800

    register and login forms | next is to edit the bootstrap designs of the login and register forms

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '163636029a1361627800422b1aa32e30'
 
 posts = [
     {
@@ -25,5 +28,53 @@ def index():
 def about():
     return render_template('about.html', title='ABOUT PAGE')
 
+@app.route('/register', methods=['GET','POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account is created for {form.username.data}!', 'success')
+        return redirect(url_for('index'))
+    return render_template('register.html', form=form, title='Register')
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', form=form, title='Login')
+
+
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
