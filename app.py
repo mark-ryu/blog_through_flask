913c370fed6d12850a0e2d59b2a0a01677554f6f
markryu <atienza.new@gmail.com>
2019-02-08 18:07:16 +0800

    database added(sqlite) | next: Package structure

--- app.py
+++ app.py
@@ -4,18 +4,32 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = '163636029a1361627800422b1aa32e30'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
+# engine can be used for the database url/uri but this could work too
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
+"""
+<<<<< creating models >>>>>>>
+passing in the db.Model as to mention that we are passing the database instance of the sqlalchemy app
+
+simply save the types of data you want into variables and using arguments required.
+
+the db.Column / db.<type of data> / primary_key / nullable / unique / etc. . . 
+
+relationship for database, backref is to get the user who created the post, when sqlalchemy loads data necessary in one go
+
+"""
+
+# for the User database
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
 
     def __repr__(self):
         return f"User('{self.username}','{self.email}','{self.image_file}')"
@@ -25,11 +39,14 @@ class Post(db.Model):
     title = db.Column(db.String(100), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
+    # the user id is for the author
+    # in the user model, the post class is used, user.id is for the table name
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
         return f"Post('{self.title}','{self.date_posted}')"
 
-
+# dummy data
 posts = [
     {
         'author': 'Arvin',
@@ -53,6 +70,14 @@ def index():
 def about():
     return render_template('about.html', title='ABOUT PAGE')
 
+"""
+<<<<<<<Register and Login Ifs>>>>>>>
+
+form is passed as and argument for the html to render
+if the form is valid, then use the flash to send a feedback message
+redirect is when the process is a success the redirected url will be shown to the user
+
+"""
 @app.route('/register', methods=['GET','POST'])
 def register():
     form = RegistrationForm()
@@ -60,6 +85,7 @@ def register():
         flash(f'Account is created for {form.username.data}!', 'success')
         return redirect(url_for('index'))
     return render_template('register.html', form=form, title='Register')
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -73,39 +99,33 @@ def login():
     return render_template('login.html', form=form, title='Login')
 
 
+
 if __name__ == "__main__":
     app.run(debug=True)
 
 
+"""
+
+<<<<<< TERMINAL COMMANDS >>>>>>
+import db as an instance since <db> is the variable used to communicate to the database using sqlalchemy
+- create all the structure of the database
+db.create_all()
+
+variable = <Model Class> ( insert the parameters )
+to save the data into the database 
+db.session.add(the_variable)
+db.session.commit()
+db.drop.all()
+
+user.id is for the user_id because it is used as the foreign key
+>>>>>>>>>>>>>>>
+<QUERY>
+
+User.query.all()
+User.query.filter_by(username='the username').all()
+User.query.get(1)
+
+>>>>>>>>>>>>
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+"""
