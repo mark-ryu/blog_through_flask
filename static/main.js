4f347c18d2110c5bb3708d97a118999feb4ef278
markryu <atienza.new@gmail.com>
2019-02-08 09:54:11 +0800

    added updated requirements.txt

---
+++ static/main.js
@@ -0,0 +1,12 @@
+<script>
+$(document).ready(function() {
+    $( ".nav-item" ).bind( "click", function(event) {
+        event.preventDefault();
+        var clickedItem = $( this );
+        $( ".nav-item" ).each( function() {
+            $( this ).removeClass( "active" );
+        });
+        clickedItem.addClass( "active" );
+    })
+});
+</script>
